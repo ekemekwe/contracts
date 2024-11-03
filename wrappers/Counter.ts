7b9ff7044eac9ba867a027e65dcee2fb09679e5a
ekemekwe <prokason@gmail.com>
2024-11-03 07:51:23 +0100

    deploy trying

--- wrappers/Counter.ts
+++ wrappers/Counter.ts
@@ -1,29 +1,22 @@
- import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Contract, ContractProvider, Sender, Address, Cell, contractAddress, beginCell } from "@ton/core";
 
-export type CounterConfig = {};
+export default class Counter implements Contract {
 
-export function counterConfigToCell(config: CounterConfig): Cell {
-    return beginCell().endCell();
-}
+  static createForDeploy(code: Cell, initialCounterValue: number): Counter {
+    const data = beginCell()
+      .storeUint(initialCounterValue, 64)
+      .endCell();
+    const workchain = 0; // deploy to workchain 0
+    const address = contractAddress(workchain, { code, data });
+    return new Counter(address, { code, data });
+  }
+  
+  constructor(readonly address: Address, readonly init?: { code: Cell, data: Cell }) {}
 
-export class Counter implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
-
-    static createFromAddress(address: Address) {
-        return new Counter(address);
-    }
-
-    static createFromConfig(config: CounterConfig, code: Cell, workchain = 0) {
-        const data = counterConfigToCell(config);
-        const init = { code, data };
-        return new Counter(contractAddress(workchain, init), init);
-    }
-
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
-        });
-    }
+  async sendDeploy(provider: ContractProvider, via: Sender) {
+    await provider.internal(via, {
+      value: "0.01", // send 0.01 TON to contract for rent
+      bounce: false
+    });
+  } 
 }
